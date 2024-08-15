b881d2e4abc7759031a3c32ff0aac2ad3afaf00d
Yevhen <zheka4573@gmail.com>
2024-08-15 11:46:22 +0300

    adding tailwind and training vue

--- src/App.vue
+++ src/App.vue
@@ -1,44 +1,6 @@
-<script>
-import { ref } from 'vue';
-
-export default {
-  setup() {
-    const name = ref('Jon Doe');
-    const status = ref('active');
-    const tasks = ref(['Task one', 'Task two', 'task three']);
-
-    const toggleStatus = () => {
-      if (status.value === 'active') {
-        status.value = 'pending';
-      } else if (status.value === 'pending') {
-        status.value = 'inactive';
-      } else {
-        status.value = 'active';
-      }
-    }
-    return {
-      name,
-      status,
-      tasks,
-      toggleStatus,
-    }
-  }
-}
-</script>
-
 <template>
-  <h1>{{ name }}</h1>
-  <br>
-  <p v-if="status === 'active'">User is active!</p>
-  <p v-else-if="status === 'pending'">User is panding</p>
-  <p v-else="status">User is inactive!</p>
+  <h1 className="text-3xl font-bold underline text-gray-500">
+      Hello world!
+    </h1>
 
-  <h3>Tasks:</h3>
-  <ul>
-    <li v-for="task in tasks" :key="task">{{ task }}</li>
-  </ul>
-
-  <br>
-  <!--<button v-on:click="toggleStatus">Change Status</button>-->
-  <button @click="toggleStatus">Change Status</button>
 </template>
