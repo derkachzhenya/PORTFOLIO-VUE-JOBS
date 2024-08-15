073c3b597b386b759f1f3a6ac4be027e1669253c
Yevhen <zheka4573@gmail.com>
2024-08-15 22:15:14 +0300

    creating app

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
-<template>
-  <h1 className="text-3xl font-bold underline text-gray-500">
-      Hello world!
-    </h1>
+<script setup>
+import Navbar from '@/components/Navbar.vue';
+import Hero from '@/components/Hero.vue';
+import HomeCards from '@/components/HomeCards.vue';
+import JobListings from '@/components/JobListings.vue';
+</script>
 
+<template>
+  <Navbar />
+  <Hero/>
+  <HomeCards/>
+  <JobListings/>
 </template>
