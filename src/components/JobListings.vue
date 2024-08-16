b3dcfbbbdf18b49baa24498a21a3cc8299b7ebaa
Yevhen <zheka4573@gmail.com>
2024-08-16 11:25:53 +0300

    creating router

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -28,7 +28,7 @@ const jobs = ref(jobData);
     </section>
 
     <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
-        <a href="/jobs" class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">View All
-            Jobs</a>
+        <router-link to="/jobs" class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700">View All
+            Jobs</router-link>
     </section>
 </template>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ import './style.css';
 import './assets/main.css';
 import 'primeicons/primeicons.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
 
-createApp(App).mount('#app')
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router'; // Путь к вашему маршрутизатору
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
